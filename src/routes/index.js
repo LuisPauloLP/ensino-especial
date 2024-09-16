329a45f9ab97b4765012667bb38a4bdacd2f48a7
Miguel-Lumertz-Espindola <migueldpa1227@gmail.com>
2024-09-16 20:44:05 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const eventsRoutes = require('./eventsRoutes') //Luis Paulo
-// const herosRoutes = require('./herosRoutes')
+const studentsRoutes = require('./studentsRoutes') //Miguel
 // const countriesRoutes = require('./countriesRoutes')
 
 router.use(express.json())
 router.use('/eventos', eventsRoutes) //Luis Paulo
-// router.use('/heros', herosRoutes)
+router.use('/estudantes', studentsRoutes) // Miguel
 // router.use('/countries', countriesRoutes)
 
 module.exports = router
-
-
-
-
-
