2cb6162fba90f8e8b7a7ac103db42234897d4174
LuisPauloLP <luisfivepaulo@unesc.net>
2024-09-23 17:08:58 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
+
+app.use(cors());
+
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
 const routes = require('./routes')
 
-app.use(function(req, res, next){ //
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-   }); //
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//swagger
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API de Gestão de Ensino Especial",
+			version: "1.0.0",
+			description: `API para demonstração de Documentação API via Swagger.  
+            
+            ### TD 01    
+            Disciplina: Desenvolvimento de Alicações II 2024.09 Turma 01  
+            Equipe: Luís Paulo, Maria Fernanda Esteves, Miguel Lumertz, Natali Elias e Rafaela Nicoski   
+			`,
+      license: {
+        name: 'Licenciado para Desenvolvimento de Aplicações II',
+      },
+      contact: {
+        name: 'Luís Paulo, Maria Fernanda Esteves, Miguel Lumertz, Natali Elias e Rafaela Nicoski'
+      },
+		},
+		servers: [
+			{
+				url: "http://localhost:3000/",
+        description: 'Server de desenvolvimento',
+			},
+		],
+	},
+	apis: [__dirname + "/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
 
 app.use('/', routes)
+//swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-app.listen(7070, function () { 
-    console.log('Aplicação executando na porta 7070!'); }); 
+
+
+app.listen(port, () => {
+  console.log(`Server rodando em http://${hostname}:${port}/api-docs`)
+})
