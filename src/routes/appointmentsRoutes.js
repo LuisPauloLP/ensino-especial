858139cc7a5326e829f157fadbac3ba3e5306793
mafe2205 <120610736+mafe2205@users.noreply.github.com>
2024-09-28 19:00:23 -0300

    Add files via upload

---
+++ src/routes/appointmentsRoutes.js
@@ -0,0 +1,378 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs');
+
+var appointmentsDB = loadAppointments();
+
+// Função carrega estudantes a partir do arquivo JSON
+function loadAppointments() {
+    try {
+      return JSON.parse(fs.readFileSync('./src/db/appointments.json', 'utf8'));
+    } catch (err) {
+      return [];
+    }
+}
+// Função para salvar os estudantes no arquivo JSON
+function saveAppointments() {
+    try {
+      fs.writeFileSync('./src/db/appointments.json', JSON.stringify(appointmentsDB, null, 2));
+      return "Saved"
+    } catch (err) {
+      return "Not saved";
+    }
+}
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Appointment:
+ *       type: object
+ *       required:
+ *         - id
+ *         - specialty
+ *         - comments
+ *         - date
+ *         - student
+ *         - professional
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: O id é gerado automáticamente pelo cadastro do agendamento
+ *         specialty:
+ *           type: string
+ *           description: Especialidade do profissional
+ *         comments:
+ *           type: string
+ *           description: Comentário sobre o que vai ser o agendamento
+ *         date:
+ *           type: date
+ *           description: Data do agendamento
+ *         student:
+ *           type: string
+ *           description: Nome do estudante
+ *         professional:
+ *           type: string
+ *           description: Nome do profissional
+ *       example:
+ *         id: 2571db14-0616-4886-aaa4-4517bb0988e8
+ *         specialty: Fisioterapeuta
+ *         comments: Sessão
+ *         date: 2024-11-24 15:00:00
+ *         student: Eduarda Fernandes
+ *         professional: Guilherme Baltezar
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Appointments
+  *   description: 
+  *     API de Controle de Agendamentos
+  *     **Por Maria Machado**
+  */
+
+ /**
+ * @swagger
+ * /appointments:
+ *   get:
+ *     summary: Retorna uma lista de todos os agendamentos
+ *     tags: [Appointments]
+ *     responses:
+ *       200:
+ *         description: A lista de agendamentos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Appointment'
+ */
+//GET todos os agendamentos
+router.get('/', (req, res) =>{
+    appointmentsDB = loadAppointments();
+    res.json(appointmentsDB);
+})
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *   get:
+ *     summary: Retorna um agendamento pelo ID
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do agendamento
+ *     responses:
+ *       200:
+ *         description: Um agendamento pelo ID
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Appointment'
+ *       404:
+ *         description: Agendamento não encontrado!
+ */
+// GET agendamento especifico por id
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    appointmentsDB = loadAppointments();
+    var appointments = appointmentsDB.find((appointments) => appointments.id === id)
+    if(!appointments) return res.status(404).json({
+        "erro": "Agendamento não encontrado!"
+    })
+    res.json(appointments)
+})
+
+/**
+ * @swagger
+ * /appointments/professional/{professional}:
+ *   get:
+ *     summary: Retorna um agendamento pelo profissional
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: professional
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Profissional do agendamento
+ *     responses:
+ *       200:
+ *         description: Um agendamento pelo profissional
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Appointment'
+ *       404:
+ *         description: Agendamento com esse profissional não encontrado!
+ */
+// GET agendamento especifico por nome do profissional
+router.get('/professional/:professional', (req, res) => {
+    const professional = req.params.professional
+    appointmentsDB = loadAppointments();
+    var appointments = appointmentsDB.filter((appointments) => appointments.professional === professional)
+    if(appointments.length === 0) return res.status(404).json({
+        "erro": "Agendamento com esse profissional não encontrado!"
+    })
+    res.json(appointments)
+})
+
+/**
+ * @swagger
+ * /appointments/student/{student}:
+ *   get:
+ *     summary: Retorna um agendamento pelo estudante
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: student
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Estudante do agendamento
+ *     responses:
+ *       200:
+ *         description: Um agendamento pelo estudante
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Appointment'
+ *       404:
+ *         description: Agendamento com esse estudante não encontrado!
+ */
+// GET personagem especifico por nome do estudante
+router.get('/student/:student', (req, res) => {
+    const student = req.params.student
+    appointmentsDB = loadAppointments();
+    var appointments = appointmentsDB.filter((appointments) => appointments.student === student)
+    if(appointments.length === 0) return res.status(404).json({
+        "erro": "Agendamento com esse estudante não encontrado!"
+    })
+    res.json(appointments)
+})
+
+/**
+ * @swagger
+ * /appointments/date/{date}:
+ *   get:
+ *     summary: Retorna um agendamento pela data
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: date
+ *         schema:
+ *           type: string
+ *           format: date
+ *         required: true
+ *         description: Data do agendamento
+ *     responses:
+ *       200:
+ *         description: Um agendamento pela data
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Appointment'
+ *       404:
+ *         description: Agendamento com essa data não encontrado!
+ */
+// GET personagem especifico por data
+router.get('/date/:date', (req, res) => {
+    const date = req.params.date
+    appointmentsDB = loadAppointments();
+    var appointments = appointmentsDB.filter((appointments) => appointments.date === date)
+    if(appointments.length === 0) return res.status(404).json({
+        "erro": "Agendamento com essa data não encontrado!"
+    })
+    res.json(appointments)
+})
+
+/**
+ * @swagger
+ * /appointments:
+ *   post:
+ *     summary: Cria um novo agendamento
+ *     tags: [Appointments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Appointment'
+ *     responses:
+ *       200:
+ *         description: O agendamento foi criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Appointment'
+ *       404_specialty_not_found:
+ *         description: Agendamento precisa ter uma 'especialidade'!
+ *       404_comments_not_found:
+ *         description: Agendamento precisa ter uma 'comentário'!
+ *       404_date_not_found:
+ *         description: Agendamento precisa ter uma 'data'!
+ *       404_student_not_found:
+ *         description: Agendamento precisa ter um 'estudante'!
+ *       404_professional_not_found:
+ *         description: Agendamento precisa ter um 'profissional'!
+ */
+// POST cadastro de agendamentos
+router.post('/', (req, res) => {
+    const newAppointment = {
+        id: uuidv4(),
+        ...req.body
+    }
+    appointmentsDB = loadAppointments();
+    if(!newAppointment.specialty) return res.status(400).json({
+        "erro": "Agendamento precisa ter uma 'especialidade'!"
+    })
+    if(!newAppointment.comments) return res.status(400).json({
+        "erro": "Agendamento precisa ter uma 'comentário'!"
+    })
+    if(!newAppointment.date) return res.status(400).json({
+        "erro": "Agendamento precisa ter uma 'data'!"
+    })
+    if(!newAppointment.student) return res.status(400).json({
+        "erro": "Agendamento precisa ter um 'estudante'!"
+    })
+    if(!newAppointment.professional) return res.status(400).json({
+        "erro": "Agendamento precisa ter um 'profissional'!"
+    })
+    appointmentsDB.push(newAppointment)
+    let resultado = saveAppointments();
+    console.log(resultado)
+    return res.json(newAppointment)
+})
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *  put:
+ *    summary: Atualiza um agendamento pelo ID
+ *    tags: [Appointments]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID do agendamento
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Appointment'
+ *    responses:
+ *      200:
+ *        description: O agendamento foi atualizado com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Appointment'
+ *      404:
+ *        description: Agendamento não encontrado!
+ */
+// UPDATE do registro nas funções
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const newAppointment = req.body 
+    appointmentsDB = loadAppointments();
+    const currentAppointment = appointmentsDB.find((appointments) => appointments.id === id)
+    const currentIndex = appointmentsDB.findIndex((appointments) => appointments.id === id)
+    if(!currentAppointment) 
+        return res.status(404).json({
+        "erro": "Agendamento não encontrado!"
+    })
+
+    appointmentsDB[currentIndex]  = newAppointment 
+    let resultado = saveAppointments();
+    console.log(resultado)
+    return res.json(newAppointment)
+})
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *   delete:
+ *     summary: Remove um agendamento pelo ID
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do agendamento
+ *     responses:
+ *       200:
+ *         description: O agendamento foi removido com sucesso
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Appointment'
+ *       404:
+ *         description: Agendamento não encontrado!
+ */
+// DELETE cadastro de agendamentos
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    appointmentsDB = loadAppointments();
+    const appointments = appointmentsDB.find((appointments) => appointments.id === id)
+    const index = appointmentsDB.findIndex((appointments) => appointments.id === id)
+    if(!appointments) return res.status(404).json({
+        "erro": "Agendamento não encontrado!"
+    })
+    var deletado = appointmentsDB.splice(index, 1)
+    let resultado = saveAppointments();
+    console.log(resultado)
+    res.json(deletado)
+})
+
+module.exports = router
---
+++ src/routes/professionalsRoutes.js
@@ -0,0 +1,312 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs');
+
+var professionalsDB = loadProfessionals();
+
+// Função carrega estudantes a partir do arquivo JSON
+function loadProfessionals() {
+    try {
+      return JSON.parse(fs.readFileSync('./src/db/professionals.json', 'utf8'));
+    } catch (err) {
+      return [];
+    }
+}
+// Função para salvar os estudantes no arquivo JSON
+function saveProfessionals() {
+    try {
+      fs.writeFileSync('./src/db/professionals.json', JSON.stringify(professionalsDB, null, 2));
+      return "Saved"
+    } catch (err) {
+      return "Not saved";
+    }
+}
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Professional:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - specialty
+ *         - contact
+ *         - phone_number
+ *         - status
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: O id é gerado automáticamente pelo cadastro do profissional
+ *         name:
+ *           type: string
+ *           description: Nome do Profissional
+ *         specialty:
+ *           type: string
+ *           description: Especialidade do profissional
+ *         contact:
+ *           type: string
+ *           description: Email do profissional
+ *         phone_number:
+ *           type: string
+ *           description: Número de contato do profissional
+ *         status:
+ *           type: string
+ *           description: Status do profissional
+ *       example:
+ *         id: 44d9ee5a-86cc-4264-a372-59b4e229aca4
+ *         name: Carlos Almeida
+ *         specialty: Pediatra
+ *         contact: calmeida.ped@gmail.com
+ *         phone_number: 48 9584 4585
+ *         status: on
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Professionals
+  *   description: 
+  *     API de Controle de Profissionais
+  *     **Por Maria Machado**
+  */
+
+ /**
+ * @swagger
+ * /professionals:
+ *   get:
+ *     summary: Retorna uma lista de todos os profissionais
+ *     tags: [Professionals]
+ *     responses:
+ *       200:
+ *         description: A lista de profissionais
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Professional'
+ */
+//GET todos os profissionais
+router.get('/', (req, res) =>{
+    professionalsDB = loadProfessionals();
+    res.json(professionalsDB);
+})
+
+/**
+ * @swagger
+ * /professionals/{id}:
+ *   get:
+ *     summary: Retorna um professional pelo ID
+ *     tags: [Professionals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do profissional
+ *     responses:
+ *       200:
+ *         description: Um profissional pelo ID
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professional'
+ *       404:
+ *         description: Profissional não encontrado!
+ */
+// GET profissionais especifico por id
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    professionalsDB = loadProfessionals();
+    var profissional = professionalsDB.find((professionals) => professionals.id === id)
+    if(!profissional) return res.status(404).json({
+        "erro": "Profissional não encontrado!"
+    })
+    res.json(profissional)
+})
+
+
+/**
+ * @swagger
+ * /professionals/name/{name}:
+ *   get:
+ *     summary: Retorna um profissional pelo nome
+ *     tags: [Professionals]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Nome do profissional
+ *     responses:
+ *       200:
+ *         description: Um profissional pelo nome
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professional'
+ *       404:
+ *         description: Profissional não encontrado!
+ */
+// GET profissional especifico por nome
+router.get('/name/:name', (req, res) => {
+    const name = req.params.name
+    professionalsDB = loadProfessionals();
+    var profissional = professionalsDB.filter((professionals) => professionals.name === name)
+    if(profissional.length === 0) return res.status(404).json({
+        "erro": "Profissional não encontrado!"
+    })
+    res.json(profissional)
+})
+
+/**
+ * @swagger
+ * /professionals:
+ *   post:
+ *     summary: Cria um novo profissional
+ *     tags: [Professionals]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Professional'
+ *     responses:
+ *       200:
+ *         description: O profissional foi criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professional'
+ *       404_name_not_found:
+ *         description: Profissional precisa ter um 'nome'!
+ *       404_specialty_not_found:
+ *         description: Profissional precisa ter uma 'especialidade'!
+ *       404_contact_not_found:
+ *         description: Profissional precisa ter uma 'email de contato'!
+ *       404_phonenumber_not_found:
+ *         description: Profissional precisa ter uma 'número de contato'!
+ *       404_status_not_found:
+ *         description: Profissional precisa ter um 'status'!
+ */
+// POST cadastro de profissionais
+router.post('/', (req, res) => {
+    const newProfessional = {
+        id: uuidv4(),
+        ...req.body
+    }
+    professionalsDB = loadProfessionals();
+    if(!newProfessional.name) return res.status(400).json({
+        "erro": "Profissional precisa ter um 'nome'!"
+    })
+    if(!newProfessional.specialty) return res.status(400).json({
+        "erro": "Profissional precisa ter uma 'especialidade'!"
+    })
+    if(!newProfessional.contact) return res.status(400).json({
+        "erro": "Profissional precisa ter uma 'email de contato'!"
+    })
+    if(!newProfessional.phone_number) return res.status(400).json({
+        "erro": "Profissional precisa ter uma 'número de contato'!"
+    })
+    if(!newProfessional.status) return res.status(400).json({
+        "erro": "Profissional precisa ter um 'status'!"
+    })
+    professionalsDB.push(newProfessional)
+    let resultado = saveProfessionals();
+    console.log(resultado)
+    return res.json(newProfessional)
+})
+
+
+/**
+ * @swagger
+ * /professionals/{id}:
+ *  put:
+ *    summary: Atualiza um professional pelo ID
+ *    tags: [Professionals]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID do profissional
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Professional'
+ *    responses:
+ *      200:
+ *        description: O profissional foi atualizado com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Professional'
+ *      404:
+ *        description: Profissional não encontrado!
+ */
+// UPDATE do registro nas funções
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const newProfessional = req.body 
+    professionalsDB = loadProfessionals();
+    const currentProfessional = professionalsDB.find((professionals) => professionals.id === id)
+    const currentIndex = professionalsDB.findIndex((professionals) => professionals.id === id)
+    if(!currentProfessional) 
+        return res.status(404).json({
+        "erro": "Profissional não encontrado!"
+    })
+
+    professionalsDB[currentIndex]  = newProfessional 
+    let resultado = saveProfessionals();
+    console.log(resultado)
+    return res.json(newProfessional)
+})
+
+/**
+ * @swagger
+ * /professionals/{id}:
+ *   delete:
+ *     summary: Remove um profissional pelo ID
+ *     tags: [Professionals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do profissional
+ *     responses:
+ *       200:
+ *         description: O profissional foi removido com sucesso
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Professional'
+ *       404:
+ *         description: Profissional não encontrado!
+ */
+// DELETE cadastro de profissionais
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    professionalsDB = loadProfessionals();
+    const professional = professionalsDB.find((professionals) => professionals.id === id)
+    const currentIndex = professionalsDB.findIndex((professionals) => professionals.id === id)
+    if(!professional) return res.status(404).json({
+        "erro": "Profissional não encontrado!"
+    })
+    var deletado = professionalsDB.splice(currentIndex, 1)
+    let resultado = saveProfessionals();
+    console.log(resultado)
+    res.json(deletado)
+})
+
+
+module.exports = router
