9ff9909786b8b433b87a372bc92eb2fd24e930c4
LuisPauloLP <luisfivepaulo@unesc.net>
2024-09-30 19:56:20 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ const eventsRoutes = require('./eventsRoutes') //Luis Paulo
 const studentsRoutes = require('./studentsRoutes') //Miguel
 const teachersRoutes = require('./teachersRoutes') //Natali 
 const appointmentsRoutes = require('./appointmentsRoutes') //Maria
-const professionalsRoutes = require('./professionalsRoutes') // ?
+const professionalsRoutes = require('./professionalsRoutes') // Maria
+const users = require('./users') // Rafaela
 
 router.use(express.json())
 router.use('/events', eventsRoutes) //Luis Paulo
 router.use('/students', studentsRoutes) // Miguel
 router.use('/teachers', teachersRoutes) // Natali
 router.use('/appointments', appointmentsRoutes) // Maria
-router.use('/professionals', professionalsRoutes) // ?
+router.use('/professionals', professionalsRoutes) // Maria
+router.use('/users', users) // Rafaela
 
 module.exports = router
